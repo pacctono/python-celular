3ee3976967856573bf1ed01e6227e1e93e7e45ec
Pablo Caraballo <pacctono@gmail.com>
2019-01-08 20:06:37 -0400

    Primera version funcional de 'convOracle2mariaDB.py'

--- convOracle2mariaDB.py
+++ convOracle2mariaDB.py
@@ -26,6 +26,16 @@ else:
 
 from lib import ES, Const as CO
 
+import re
+def ireplace(self, viejo, nuevo, count=0):
+  ''' Se comporta como string.replace(), pero lo hace
+      sin importar minusculas o mayusculas. '''
+  
+# Las dos lineas siguientes pueden ser sustituidas por:
+#  return re.sub('(?i)'+re.escape(viejo), nuevo, self)
+  patron = re.compile(re.escape(viejo), re.I) # re.I:ignora minus/mayus
+  return re.sub(patron, nuevo, self, count)
+# FIN funcion ireplace
 if bMovil:
   def cargarNombres(nombArchEnt='nomina*.sql'):
     rutaDatos = DIR
@@ -48,6 +58,35 @@ if bMovil:
     if None == indice or 0 > indice: return None
     return(lFiles[indice])
   # FIN funcion buscarArchivo
+def obtenerFecha(fechaConFormato):
+  fecha, formato = fechaConFormato.split(',')
+  fecha = fecha.strip("' \"")
+  formato = formato.strip("' \"")
+  iDia = formato.find('d')
+  if 0 <= iDia: nDia = formato.count('d')
+  iMes = formato.find('m')
+  if 0 <= iMes: nMes = formato.count('m')
+  iAno = formato.find('y')
+  if 0 <= iAno: nAno = formato.count('y')
+  if 0 > iDia or 0 > iMes or 0 > iAno: return "'" + fecha + "'"
+  else:
+    return "'" + fecha[iAno:iAno+nAno] + '-' + fecha[iMes:iMes+nMes] + \
+            '-' + fecha[iDia:iDia+nDia] + " 00:00:00'"
+# FIN funcion fechaFormateada
+def tipoNumber(linea):
+  if '(' in linea: return ireplace(linea, 'NUMBER', 'DECIMAL')
+  else: return ireplace(linea, 'NUMBER', 'DOUBLE')
+# FIN funcion tipoNumber
+def tipoVarchar2(linea):
+  linea = ireplace(linea, 'VARCHAR2', 'VARCHAR')    
+  if ' char' in linea.lower():
+    iChar = linea.lower().index(' char')
+    linea = linea[0:iChar] + linea[iChar+5:]
+  return linea
+# FIN funcion tipoVarchar2
+def tipoDate(linea):
+  return ireplace(linea, 'date', 'DATETIME')
+# FIN funcion tipoDate
 
 sCed = None
 if bMovil:
@@ -64,7 +103,7 @@ else:
         prefijoNombArch = sys.argv[3]
     else:
       if sufijoEntrada.isdigit():
-        if 4 <= len(sufijoEntrada) and 6 >= len(sufijoEntrada):
+        if 4 <= len(sufijoEntrada) <= 6:
           if 4 == len(sufijoEntrada):
             sufijoSalida = '20' + sufijoEntrada[2:] + '_' + \
                             sufijoEntrada[0:2]
@@ -103,61 +142,71 @@ while True:
 #  print("%sNombre de archivos, entrada:%s '%s'%s, salida:%s '%s'" % \
 #        (CO.AMARI, CO.FIN, nombArchEntCompleto, CO.AMARI, CO.FIN, \
 #          nombArchSalCompleto))
-  nInsert = False   # No se ha encontrado un 'insert into'.
   salida  = []
   salida.append('SET GLOBAL max_allowed_packet=128*1024*1024;')
   nLineas = 1       # Numero de lineas
+  nInsert = False   # No se ha encontrado un 'insert into'.
+  crearTabla = False
+  linUltValue = []
   for linea in f:
     linea = linea[0:linea.index('\n')]
-    if 0 <= linea.lower().find('set feed'): continue  # set feedback
-    if 0 <= linea.lower().find('alter') and \
-      0 <= linea.lower().find('triggers'): continue   # set alter.*triggers
-    if 0 <= linea.lower().find('set define'): continue  # set define
-    if 0 <= linea.lower().find('prompt'): continue    # prompt
-    if 0 <= linea.lower().find('commit'): continue    # commit
-    if 0 <= linea.find('NOMINA'):
-      linea = linea.replace('NOMINA', 'nomina')
-    if 0 <= linea.find('PERSONAL'):
-      linea = linea.replace('PERSONAL', 'personal')
-    if 0 <= linea.find('RAC'):
-      linea = linea.replace('RAC', 'rac')
-    if 0 <= linea.lower().find('create table'):
+    if 'set feed' in linea.lower(): continue  # set feedback
+    if 'alter' in linea.lower() and \
+      'triggers' in linea.lower(): continue   # set alter.*triggers
+    if 'set define' in linea.lower(): continue   # set define
+    if 'prompt' in linea.lower(): continue       # prompt
+    if 'commit' in linea.lower(): continue    # commit
+    if 'NOMINA' in linea: linea = linea.replace('NOMINA', 'nomina')
+    if 'PERSONAL' in linea:
+          linea = linea.replace('PERSONAL', 'personal')
+    if 'RAC' in linea: linea = linea.replace('RAC', 'rac')
+    if crearTabla:
+      if ';' in linea: crearTabla = False
+      if 'number' in linea.lower(): linea = tipoNumber(linea.rstrip())
+      if 'date' in linea.lower(): linea = tipoDate(linea.rstrip())
+      if 'varchar2' in linea.lower():
+        linea = tipoVarchar2(linea.rstrip())
+    if 'create table' in linea.lower():
       linea = linea.replace(sufijoEntrada, sufijoSalida)
       salida.append('DROP TABLE IF EXISTS ' + \
           linea[linea.lower().find('create')+13:] + ';')
       nLineas += 1
       linea = linea.replace('create table', 'CREATE TABLE')
-    if 0 <= linea.lower().find('create unique index'):
+      crearTabla = True
+    if 'create unique index' in linea.lower():
       linea = linea.replace(sufijoEntrada, sufijoSalida)
       linea = linea.replace('create unique index', \
                             'CREATE UNIQUE INDEX')
-    if 0 <= linea.lower().find('insert into'):
+    if 'insert into' in linea.lower():
       if not nInsert: # Mantener y cambiar primera linea con 'insert into'
         linea = linea.replace(sufijoEntrada, sufijoSalida)
-        nInsert = True
         linea = linea.lower().replace('insert into', 'INSERT INTO')
         linea = linea[0:linea.index('(')] + 'VALUES'
-        linUltValue = nLineas + 1   # El '1' representa la linea actual.
+        nInsert = True
       else:
         continue
-    if nInsert and 0 == linea.lower().find('values '):
-      if 0 < linea.rfind(';'): linea = linea[0:linea.rfind(';')] + ','
-      linea = linea.replace('values ', '')    
-      linea = linea.replace('VALUES ', '')    
-      if 0 < linea.lower().find('to_date'):
-        iDate = linea.lower().index('to_date')
-        iParentesisAbre = linea.index('(', iDate)
-        iParentesisCierra = linea.index(')', iParentesisAbre)
-        fecha = \
+    if nInsert and 'values ' in linea.lower():
+      if 0 == linea.lower().find('values '):
+        if 0 < linea.rfind(';'): linea = linea[0:linea.rfind(';')] + ','
+        linea = ireplace(linea, 'values ', '')    
+        if 'to_date' in linea.lower():
+          iDate = linea.lower().index('to_date')
+          iParentesisAbre = linea.index('(', iDate)
+          iParentesisCierra = linea.index(')', iParentesisAbre)
+          fechaConFormato = \
             linea[iParentesisAbre+1:iParentesisCierra].lstrip().rstrip()
-        linea = linea[0:iDate] + fecha + \
-                linea[iParentesisCierra+1:].lstrip().rstrip()
-      linUltValue += 1
+          fecha = obtenerFecha(fechaConFormato)
+          linea = linea[0:iDate] + fecha + \
+                  linea[iParentesisCierra+1:].lstrip().rstrip()
+      else:
+        nInsert = False
+        linUltValue.append(nLineas)
     nLineas += 1
     salida.append(linea)
-  linUltValue -= 1
-  salida[linUltValue] = \
-            salida[linUltValue][0:salida[linUltValue].rfind(',')] + ';'
+  # FIN del for linea in f:
+  if 0 >= len(linUltValue): linUltValue.append(nLineas - 1)
+  for l in linUltValue:
+    salida[l] = salida[l][0:salida[l].rfind(',')] + ';'
   for linea in salida:
     print(linea)
 #  print(linUltValue)
